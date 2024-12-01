ac1adeeffa0168ab8942de59cc898e7743eac1cb
RakeshSangi <46862543+RakeshSangi@users.noreply.github.com>
2024-12-01 22:22:08 +0530

    Update variables.tf

--- Day-3/mymodules/instance/variables.tf
+++ Day-3/mymodules/instance/variables.tf
@@ -1,1 +1,11 @@
+variable "aws_instance_type_value" {
+    description = "this is instance type value"
+}
 
+variable "aws_ami_value" {
+    description = "ami value"
+}
+
+variable "subnet_value" {
+   description = "subnet value"
+}
